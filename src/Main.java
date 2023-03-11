e0238027838af647f963f5c11d39a75202350f97
Lukrecja <pawelpleskaczynski@gmail.com>
2023-03-11 15:32:15 +0100

    optimized tag reading, added cli parameters

--- src/Main.java
+++ src/Main.java
@@ -7,58 +7,104 @@ import net.querz.nbt.io.NamedTag;
 import net.querz.nbt.tag.CompoundTag;
 import net.querz.nbt.tag.ListTag;
 import net.querz.nbt.tag.Tag;
+import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
-public class Main {
-    static void findBadTags(Tag<?> tag, File file, int maxDepth) {
+@CommandLine.Command(name = "nbtmcafix", description = "Finds and fixes players and entities with broken (too deep) NBT tags.")
+
+public class Main implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", description = "Path to `world` directory.")
+    private String basePath;
+
+    @CommandLine.Option(names = { "-p", "--players_depth" }, description = "Maximum allowed depth of players' NBT tags (default = 512).")
+    private int playerDepth = 512;
+
+    @CommandLine.Option(names = { "-e", "--entities_depth" }, description = "Maximum allowed depth of entities' NBT tags (default = 512).")
+    private int entityDepth = 512;
+
+    @Override
+    public Integer call() throws Exception {
+        Path playerdataPath = Paths.get(basePath, "playerdata");
+        Path entitiesPath = Paths.get(basePath, "entities");
+
+        File playerdataDir = new File(playerdataPath.toString());
+        File entitiesDir = new File(entitiesPath.toString());
+
+        File[] playerdataFiles = playerdataDir.listFiles();
+        File[] entitiesFiles = entitiesDir.listFiles();
+
+        if (playerdataFiles != null && entitiesFiles != null) {
+            for (File player : playerdataFiles) {
+                fixPlayer(player, playerDepth);
+            }
+            for (File entity : entitiesFiles) {
+                fixChunks(entity, entityDepth);
+            }
+        } else {
+            System.out.println("Invalid directories");
+            return 1;
+        }
+        return 0;
+    }
+
+    static boolean findBadTags(Tag<?> tag, File file, int maxDepth) {
         CompoundTag compoundTag = (CompoundTag) tag;
         CompoundTag nestedCompoundTag;
         ListTag<?> nestedListTag;
         Set<String> keys = compoundTag.keySet();
         String[] keyArray = keys.toArray(new String[0]);
+        boolean save = false;
 
         for (int i = 0; i < keys.size(); i++) {
             try {
                 nestedCompoundTag = compoundTag.getCompoundTag(keyArray[i]);
-                findBadTags(nestedCompoundTag, file, maxDepth);
+                save |= findBadTags(nestedCompoundTag, file, maxDepth);
             } catch (ClassCastException e) {
                 try {
                     nestedListTag = compoundTag.getListTag(keyArray[i]);
                     try {
-                        nestedListTag.valueToString(maxDepth);
+                        nestedListTag.checkDepth(maxDepth);
                     } catch (MaxDepthReachedException e2) {
-                        System.out.println("[" + file.getName() + "] Max depth reached in object " + keyArray[i]);
+                        System.out.println("\tMax depth reached in object " + keyArray[i]);
                         compoundTag.remove(keyArray[i]);
+                        save = true;
                     }
                 } catch (ClassCastException ignored) {}
             }
         }
+
+        return save;
     }
 
     static void fixChunks(File file, int maxDepth) {
         try {
+            boolean save = false;
             System.out.println("[" + file.getName() + "] Reading file");
             Chunk chunk = null;
             MCAFile mcafile = MCAUtil.read(file);
             for (int i = 0; i < 1024; i++) {
                 try {
                     chunk = mcafile.getChunk(i);
-                    chunk.getEntities().valueToString(maxDepth);
+                    chunk.getEntities().checkDepth(maxDepth);
                 } catch (MaxDepthReachedException e) {
-                    System.out.println("[" + file.getName() + "] Chunk " + i + " broken");
+                    System.out.println("[" + file.getName() + "] Chunk " + i + " contains invalid entities");
                     if (chunk != null) {
                         System.out.println("\tRemoving entities");
                         chunk.getEntities().clear();
+                        save = true;
                     }
                 } catch (NullPointerException ignored) {}
             }
-            MCAUtil.write(mcafile, file.getPath());
-            System.out.println("[" + file.getName() + "] File saved");
+            if (save) {
+                MCAUtil.write(mcafile, file.getPath());
+                System.out.println("[" + file.getName() + "] File saved");
+            }
         } catch (IOException e) {
             System.out.println("[" + file.getName() + "] File is empty");
         }
@@ -72,40 +118,18 @@ public class Main {
             NamedTag namedTag = NBTUtil.read(file);
             tag = (CompoundTag) namedTag.getTag();
 
-            findBadTags(tag, file, maxDepth);
+            if (findBadTags(tag, file, maxDepth)) {
+                NBTUtil.write(tag, file.getPath());
+                System.out.println("[" + file.getName() + "] File saved");
+            }
 
-            NBTUtil.write(tag, file.getPath());
-
-            System.out.println("[" + file.getName() + "] File saved");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Specify path to 'world' directory");
-        } else {
-            String basePath = args[0];
-            Path playerdataPath = Paths.get(basePath, "playerdata");
-            Path entitiesPath = Paths.get(basePath, "entities");
-
-            File playerdataDir = new File(playerdataPath.toString());
-            File entitiesDir = new File(entitiesPath.toString());
-
-            File[] playerdataFiles = playerdataDir.listFiles();
-            File[] entitiesFiles = entitiesDir.listFiles();
-
-            if (playerdataFiles != null && entitiesFiles != null) {
-                for (File player : playerdataFiles) {
-                    fixPlayer(player, 64);
-                }
-                for (File entity : entitiesFiles) {
-                    fixChunks(entity, 256);
-                }
-            } else {
-                System.out.println("Invalid directories");
-            }
-        }
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
